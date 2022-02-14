c63c0b8063b00928c5c997282d4623bfe28b02b9
aritra1804 <aritradutta04@gmail.com>
2022-02-14 15:22:05 +0530

    Home Page complete🏠

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,19 @@ import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-// import Newsletter from "../components/Newsletter";
-// import Products from "../components/Products";
-// import Slider from "../components/Slider";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
-      {/* <Slider /> */}
+      <Slider /> 
       <Categories />
-      {/* <Products/>
-      <Newsletter/> */}
+      <Products/>
+      <Newsletter/> 
       <Footer/>
     </div>
   );
